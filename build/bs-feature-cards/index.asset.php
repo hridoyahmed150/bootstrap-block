7d7eb62c56c55f08e12ff15bac8307f53ad938e5
hridoyahmed150 <hridoyahmed9391@gmail.com>
2025-12-25 08:19:16 +0600

    bs-feature card ready to prod

--- build/bs-feature-cards/index.asset.php
+++ build/bs-feature-cards/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '87293575d40b335f6908');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'a0b22fb96f74f2032ba8');
